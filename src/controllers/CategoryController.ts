0226ba93451cde96e8890226991b7a62ca75570b
Richard Viana <richardjuniolv@gmail.com>
2023-04-05 15:33:15 -0300

    adding categories CRUD

---
+++ src/controllers/CategoryController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from 'express';
+import prisma from 'src/database/prisma';
+import * as Yup from 'yup';
+
+interface IStoreCategory {
+  name: string;
+}
+
+class ProductController {
+  async index(req: Request, res: Response): Promise<Response> {
+    const categories = await prisma.category.findMany();
+
+    return res.json({ categories });
+  }
+
+  async store(req: Request, res: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+    });
+
+    try {
+      await schema.validate(req.body, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const validationErrors = [];
+      const messages = [];
+
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors.push(error.path);
+          messages.push(error.message);
+        });
+      }
+
+      return res.status(400).json({
+        error: 'Validation fails',
+        field: validationErrors,
+        messages,
+      });
+    }
+
+    const { name }: IStoreCategory = req.body;
+
+    const categoryExists = await prisma.category.findUnique({
+      where: { name },
+    });
+
+    if (categoryExists) {
+      return res.status(400).json({ error: 'Category already exists' });
+    }
+
+    const category = await prisma.category.create({
+      data: {
+        name,
+      },
+    });
+
+    return res.json(category);
+  }
+
+  async delete(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const category = await prisma.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+
+    await prisma.category.delete({
+      where: { id },
+    });
+
+    return res.json({ message: 'Category deleted' });
+  }
+}
+
+export default new ProductController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import CategoryController from './controllers/CategoryController';
 import ProductController from './controllers/ProductController';
 
 const router = Router();
@@ -10,5 +11,10 @@ router.get('/products/:id', ProductController.show);
 router.post('/products', ProductController.store);
 router.put('/products/:id', ProductController.update);
 router.delete('/products/:id', ProductController.delete);
+
+// Categories routes
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
+router.delete('/categories/:id', CategoryController.delete);
 
 export { router };
